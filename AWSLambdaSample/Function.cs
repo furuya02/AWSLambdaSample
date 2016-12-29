c7c2b5a4291c79300a55ada4f2fcf8d83f8b5165
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-12-29 13:30:48 +0900

    first commit

---
+++ AWSLambdaSample/Function.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace AWSLambdaSample
+{
+	public class Function
+	{
+		public string FunctionHandler(SampleData sampleData, ILambdaContext context)
+		{
+			context.Logger.LogLine(sampleData.ToString());
+			return sampleData.ToString();
+		}
+	}
+
+	public class SampleData
+	{
+		public string Key1 { get; set; }
+		public string Key2 { get; set; }
+		public string Key3 { get; set; }
+		override public string ToString()
+		{
+			return $"Key1={Key1}, Key2={Key2}, Key3={Key3}";
+		}
+	}
+}
---
+++ AWSLambdaSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Amazon.Lambda.TestUtilities;
+using AWSLambdaSample;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// setup
+		var sut = new Function();
+		var expected = "Key1=value1, Key2=value2, Key3=value3";
+		var sampleData = new SampleData() { Key1 = "value1", Key2 = "value2", Key3 = "value3" };
+
+		//exercise
+		var acture = sut.FunctionHandler(sampleData, new TestLambdaContext());
+
+		//veryfy
+		Assert.That(acture, expected);
+
+		//tearDown
+	}
+
+	class Assert
+	{
+		static public void That(string acture, string expected)
+		{
+			if (acture == expected)
+			{
+				Console.WriteLine($"success");
+			}
+			else
+			{
+				Console.WriteLine($"error {acture}!={expected}");
+			}
+		}
+	}
+}
